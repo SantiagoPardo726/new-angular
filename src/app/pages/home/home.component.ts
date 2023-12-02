9df680bc4df0de56a87950c0729ccbd7d9a64577
SantiagoPardo726 <69635343+SantiagoPardo726@users.noreply.github.com>
2023-12-01 22:28:24 -0500

    Update home and labs components

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,15 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Task } from '../../models/task.model';
-
+import { FormControl, ReactiveFormsModule,Validators } from '@angular/forms';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css',
 })
+
 export class HomeComponent {
   tasks = signal<Task[]>([
     { id: Date.now(), title: 'Crear Proyecto', completed: false },
@@ -16,11 +17,17 @@ export class HomeComponent {
     { id: Date.now(), title: 'Crear Proyecto 3', completed: false },
   ]);
 
-  changeHandler(event: Event) {
-    const input = event.target as HTMLInputElement;
-    const newTask = input.value;
-    this.addTask(newTask);
-  }
+  newTaskCtrl = new FormControl('',{
+    nonNullable: true,
+    validators: [Validators.required],
+  });
+
+  changeHandler() {
+    if(this.newTaskCtrl.valid){
+      this.addTask(this.newTaskCtrl.value);
+      this.newTaskCtrl.setValue('');
+    }
+  };
   addTask(title: string){
     const newTask: Task = {
       id: Date.now(),
--- src/app/pages/labs/labs.component.ts
+++ src/app/pages/labs/labs.component.ts
@@ -1,42 +1,63 @@
 import { Component, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-labs',
   standalone: true,
-  imports: [
-    CommonModule
-  ],
+  imports: [CommonModule,ReactiveFormsModule],
   templateUrl: './labs.component.html',
-  styleUrl: './labs.component.css'
+  styleUrl: './labs.component.css',
 })
 export class LabsComponent {
   title = 'app';
   welcome = 'Hola nuevo mundo';
-  tasks = signal([ 'Tarea 1', 'Tarea 2', 'Tarea 3' ]);
+  tasks = signal(['Tarea 1', 'Tarea 2', 'Tarea 3']);
   // name = 'Santiago';
   name = signal('Santiago');
   edad = 21;
   disable = false;
   img = 'https://www.w3schools.com/howto/img_avatar.jpg';
-  person={
+  person = signal({
     name: 'Santiago',
     age: 21,
-    avatar: 'https://www.w3schools.com/howto/img_avatar.jpg'
-  };
-  
+    avatar: 'https://www.w3schools.com/howto/img_avatar.jpg',
+  });
+
+  colorCtrl = new FormControl();
+
+  constructor() {
+    this.colorCtrl.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
+  }
+    
+
+  changeAge(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update((prevState) => {
+      return { ...prevState, age: parseInt(newValue, 10) };
+    });
+  }
+  changeName(event: Event) {
+    const input = event.target as HTMLInputElement;
+    const newValue = input.value;
+    this.person.update((prevState) => {
+      return { ...prevState, name: newValue };
+    });
+  }
 
   clickHandler() {
     alert('Hola');
   }
-  changeHandler(event:Event) {
+  changeHandler(event: Event) {
     const input = event.target as HTMLInputElement;
     const newValue = input.value;
     this.name.set(newValue);
   }
-  keydownHandler(event:KeyboardEvent) {
+  keydownHandler(event: KeyboardEvent) {
     const input = event.target as HTMLInputElement;
     console.log(input.value);
   }
-
 }
